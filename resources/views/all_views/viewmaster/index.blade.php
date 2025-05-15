063cd14a5aa4a3eb601003da8eaca303be8ffd01
GianlucaJC <morescogianluca@gmail.com>
2025-05-15 15:59:54 +0200

    update text

--- resources/views/all_views/viewmaster/index.blade.php
+++ resources/views/all_views/viewmaster/index.blade.php
@@ -153,7 +153,7 @@
                         <li><a href="javascript:void(0)" onclick="$('#about').hide();$('#div_intro').hide();$('#div_sign').show(200);">Register</a></li>
                        
                         <li><a href="contact">Contact Us</a></li>
-                        <li><a href="https://www.advanzpharma.com/privacy-policy" target='_blank'>Privacy Policies</a></li>
+                        <li><a  href="https://www.advanzpharma.com/privacy-policy" target='_blank'>Privacy Policies</a></li>
 
                      </ul>
                      @endif
@@ -362,7 +362,7 @@
                               <a href="contact">Contact Us</a>
                            </li>
                            <li>
-                           <a href="https://www.advanzpharma.com/privacy-policy" target='_blank'>Privacy Policies</a>
+                           <a onclick="if (!confirm('Dear user, you are leaving the ASTIP website, are you sure?')) event.preventDefault();" href="https://www.advanzpharma.com/privacy-policy" target='_blank'>Privacy Policies</a>
                            </li>
                         </ul>
                      </div>
