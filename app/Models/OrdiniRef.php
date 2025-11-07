3ed446fe6ecb712e764ee003a7aa7d7e0bd90413
GianlucaJC <morescogianluca@gmail.com>
2025-11-07 17:19:51 +0100

    Case sensitive

--- app/Models/OrdiniRef.php
+++ app/Models/OrdiniRef.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Uploads extends Model
+class OrdiniRef extends Model
 {
+    protected $table="ordini_ref";
     use HasFactory;
 }
--- app/Models/RuleOrder.php
+++ app/Models/RuleOrder.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Uploads extends Model
+class RuleOrder extends Model
 {
+    protected $table="rule_order";
     use HasFactory;
 }
